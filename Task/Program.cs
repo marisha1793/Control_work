35c4f851a755a975558cb94ba03d43064e0b7cef
Mariya <marisha_1793@mail.ru>
2023-01-25 15:36:47 +0700

    Decision5

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,19 +4,17 @@
 лучше обойтись исключительно массивами. */
 
 Console.WriteLine("Введем список всех элементов массива: ");
-string[]array = CreateArray();
+string[]array = new string [5] {"hello", "hi", "bye", "one", "hohoho"};
 PrintArray(array);
 Console.WriteLine();
 Console.WriteLine("Из всего списка оставим только те, чья длина не более 3 символов: ");
 string[]newArray = SortArray(array);
 PrintArray(newArray);
 
-
 string[] SortArray(string[] array)
 {
     int index = 0;
-    int number = array.Length;
-    string[] newArray = new string [number];
+    string[] newArray = new string [array.Length];
     for (var i = 0; i < array.Length; i++)
     {
         if (array[i].Length < 4)
@@ -28,22 +26,10 @@ string[] SortArray(string[] array)
     return newArray;
 }
 
-
 void PrintArray(string[]ar)
 {
     for (var i = 0; i < ar.Length; i++)
     {
         Console.Write($"{ar[i]} ");
     }
-}
-
-string[] CreateArray()
-{
-    string[] array = new string [5];
-    array[0] = "hello";
-    array[1] = "hi";
-    array[2] = "bye";
-    array[3] = "one";
-    array[4] = "hoho";
-    return array;
 }
